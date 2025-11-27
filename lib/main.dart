77ffc277de6a18d7702e70097d9d9c737256fd00
Cristian Daniel Baron Rodriguez <Baronrodriguezcristian@gmail.com>
2025-11-27 08:53:27 -0500

    Super App Cambios

--- lib/main.dart
+++ lib/main.dart
@@ -17,23 +17,21 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.deepPurpleAccent.shade700,
           title: const Text("Super App", style: TextStyle(color: Colors.white)),
         ),
-        body: Column( // Quitamos el Center porque Expanded necesita saber límites
+        body: Column(
           children: [
             const Padding(
               padding: EdgeInsets.all(20.0),
               child: Text("Componente Local (App Host)"),
             ),
 
-            // SOLUCIÓN: Usamos Expanded
-            // Esto le dice al módulo: "Toma todo el espacio sobrante de la pantalla"
             Expanded(
               child: Container(
-                clipBehavior: Clip.antiAlias, // Opcional: para redondear bordes
+                clipBehavior: Clip.antiAlias,
                 decoration: BoxDecoration(
                   border: Border.all(color: Colors.grey),
                   borderRadius: BorderRadius.circular(12),
                 ),
-                // Aquí vive tu aplicación de FlutterFlow encapsulada
+
                 child: const ModuloFlutterFlow(),
               ),
             ),
